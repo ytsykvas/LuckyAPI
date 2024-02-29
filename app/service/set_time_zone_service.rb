c45f527fa8d7d1b5ff168e9d401748ad9b104de6
Yurii Tsykvas <tsykvasyurii@gmail.com>
2024-02-29 20:45:35 +0200

    Refactored class SetTimeZoneService

--- app/service/set_time_zone_service.rb
+++ app/service/set_time_zone_service.rb
@@ -7,17 +7,29 @@ class SetTimeZoneService
 
   def get_time_zone
     time_info = { UTC: Time.now.utc }
-    cities = @params[:cities]&.split(',')
-    if cities.present?
-      cities.each do |city|
-        time_zone = ActiveSupport::TimeZone[city.strip]
-        time_info[city.strip.to_sym] = if time_zone.present?
-                                         Time.now.in_time_zone(time_zone).to_time
-                                       else
-                                         'Invalid Timezone'
-                                       end
-      end
+    cities = parse_cities(@params[:cities])
+
+    cities.each do |city|
+      time_info[city.to_sym] = get_local_time(city)
     end
+
     time_info
+  end
+
+  private
+
+  def parse_cities(cities)
+    return [] unless cities.present?
+
+    cities.split(',').map(&:strip)
+  end
+
+  def get_local_time(city)
+    time_zone = ActiveSupport::TimeZone[city]
+    if time_zone.present?
+      Time.now.in_time_zone(time_zone).to_time
+    else
+      'Invalid Timezone'
+    end
   end
 end
